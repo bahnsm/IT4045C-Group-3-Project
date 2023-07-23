c85f63594c13b21eb922772117c337606d07ea2d
Benjamin J Hicks <73999671+HennyBicks01@users.noreply.github.com>
2023-07-23 16:37:39 -0400

    landing page struggles

--- SpotifyTop/src/main/java/FinalProject/SpotifyTop/SpotifyTopTracksAndArtistsApplication.java
+++ SpotifyTop/src/main/java/FinalProject/SpotifyTop/SpotifyTopTracksAndArtistsApplication.java
@@ -2,13 +2,15 @@ package FinalProject.SpotifyTop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class SpotifyTopTracksAndArtistsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpotifyTopTracksAndArtistsApplication.class, args);
-		
+
 	}
 
 }
