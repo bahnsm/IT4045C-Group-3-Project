2521674dd2f8dab541dd20786e29247121db32d5
Benjamin J Hicks <73999671+HennyBicks01@users.noreply.github.com>
2023-07-23 11:17:29 -0400

    spring init

---
+++ SpotifyTop/src/main/java/FinalProject/SpotifyTop/SpotifyTopTracksAndArtistsApplication.java
@@ -0,0 +1,13 @@
+package FinalProject.SpotifyTop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyTopTracksAndArtistsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyTopTracksAndArtistsApplication.class, args);
+	}
+
+}
---
+++ SpotifyTop/src/test/java/FinalProject/SpotifyTop/SpotifyTopTracksAndArtistsApplicationTests.java
@@ -0,0 +1,13 @@
+package FinalProject.SpotifyTop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyTopTracksAndArtistsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
