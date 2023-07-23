d34943a203e55d894a140f495012e636ee806ab1
Benjamin J Hicks <73999671+HennyBicks01@users.noreply.github.com>
2023-07-23 16:37:27 -0400

    trying to get html landing page to load

--- SpotifyTop/src/main/java/FinalProject/SpotifyTop/AuthController.java
+++ SpotifyTop/src/main/java/FinalProject/SpotifyTop/AuthController.java
@@ -1,14 +1,22 @@
 package FinalProject.SpotifyTop;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.SpotifyHttpManager;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.model_objects.credentials.AuthorizationCodeCredentials;
+import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
 import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
 
+import java.io.IOException;
 import java.net.URI;
 
 @RestController
@@ -34,4 +42,25 @@ public class AuthController {
         return uri.toString();
     }
 
+    public String getSpotifyUserCode(@RequestParam("code") String userCode, HttpServletResponse response) throws IOException {
+        code = userCode;
+        AuthorizationCodeRequest authorizationCodeRequest = spotifyApi.authorizationCode(code)
+            .build(); 
+
+        try {
+            final AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRequest.execute();
+            
+            spotifyApi.setAccessToken(authorizationCodeCredentials.getAccessToken());
+            spotifyApi.setRefreshToken(authorizationCodeCredentials.getRefreshToken());
+            
+            System.out.println("Expires in: " + authorizationCodeCredentials.getExpiresIn());
+
+        } catch (IOException | SpotifyWebApiException | org.apache.hc.core5.http.ParseException e) {
+            System.out.println("Error: " + e.getMessage()); 
+        } 
+
+        response.sendRedirect("http://localhost:8080/index.html");
+        return spotifyApi.getAccessToken();
+    
+    }
 }
