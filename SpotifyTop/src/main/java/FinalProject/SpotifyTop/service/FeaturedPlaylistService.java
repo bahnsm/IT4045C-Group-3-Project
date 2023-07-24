2d87174803fbd5d95114066b92ab63bc3ed082fb
Benjamin J Hicks <73999671+HennyBicks01@users.noreply.github.com>
2023-07-24 18:35:07 -0400

    added comments to code

--- SpotifyTop/src/main/java/FinalProject/SpotifyTop/service/FeaturedPlaylistService.java
+++ SpotifyTop/src/main/java/FinalProject/SpotifyTop/service/FeaturedPlaylistService.java
@@ -19,12 +19,21 @@ public class FeaturedPlaylistService {
     private final RestTemplate restTemplate;
     private static final String URL = "https://api.spotify.com/v1/browse/featured-playlists?limit=50";
 
+    /**
+     * Fetches the featured playlists from the Spotify API.
+     *
+     * @param token The access token required for authentication with the Spotify API.
+     * @return A LinkedHashMap containing the featured playlists data.
+     */
     public LinkedHashMap<String, Object> getPlaylists(String token) {
+        // Set the authorization header with the access token
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Bearer " + token);
 
+        // Create an HttpEntity with the headers
         HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
 
+        // Make a GET request to the Spotify API and retrieve the response
         ResponseEntity<LinkedHashMap<String, Object>> response = restTemplate.exchange(URL, HttpMethod.GET, entity, new ParameterizedTypeReference<>() {});
 
         // Retrieve the response body directly as LinkedHashMap<String, Object>
