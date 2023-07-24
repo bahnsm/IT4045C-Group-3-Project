1bd036fb8398641dce86f9c7f7fa47307f7d9536
Benjamin J Hicks <73999671+HennyBicks01@users.noreply.github.com>
2023-07-24 18:21:27 -0400

    updated no track playing exception

--- SpotifyTop/src/main/java/FinalProject/SpotifyTop/exception/NoTrackPlayingException.java
+++ SpotifyTop/src/main/java/FinalProject/SpotifyTop/exception/NoTrackPlayingException.java
@@ -1,7 +1,9 @@
 package finalproject.spotifytop.exception;
 
+// Custom exception class that extends the RuntimeException class
 public class NoTrackPlayingException extends RuntimeException {
 
-	private static final long serialVersionUID = 5591551405532580320L;
+    // A unique identifier for serialization purposes
+    private static final long serialVersionUID = 5591551405532580320L;
 
 }
