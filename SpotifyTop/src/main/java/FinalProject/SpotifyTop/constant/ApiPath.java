e6a4d1665c90601d67f1e8bdad9e6c1b93c03037
Benjamin J Hicks <73999671+HennyBicks01@users.noreply.github.com>
2023-07-24 17:56:46 -0400

    updated API path and music controller

--- SpotifyTop/src/main/java/FinalProject/SpotifyTop/constant/ApiPath.java
+++ SpotifyTop/src/main/java/FinalProject/SpotifyTop/constant/ApiPath.java
@@ -6,29 +6,14 @@ public class ApiPath {
     // Represents the API path for the callback endpoint
     public static final String CALLBACK = "/callback";
 
-    // Represents the API path for the featured playlists endpoint
-    public static final String FEATURED_PLAYLIST = "/featuredPlaylists";
-
     // Represents the API path for the logout endpoint
     public static final String LOGOUT = "/logout";
 
     // Represents the API path for the error endpoint
     public static final String ERROR = "/error";
 
-    // Represents the API path for the new releases endpoint
-    public static final String NEW_RELEASE = "/newReleases";
-
-    // Represents the API path for the recent tracks endpoint
-    public static final String RECENT_TRACKS = "/recentTracks";
-
     // Represents the API path for the redirect endpoint
     public static final String REDIRECT = "/redirect";
-
-    // Represents the API path for the saved albums endpoint
-    public static final String SAVED_ALBUMS = "/savedAlbums";
-
-    // Represents the API path for the saved tracks endpoint
-    public static final String SAVED_TRACKS = "/savedTracks";
 
     // Represents the API path for the search endpoint
     public static final String SEARCH = "/search";
--- SpotifyTop/src/main/java/FinalProject/SpotifyTop/controller/MusicController.java
+++ SpotifyTop/src/main/java/FinalProject/SpotifyTop/controller/MusicController.java
@@ -62,43 +62,4 @@ public class MusicController {
 		return Template.TOP_ARTISTS;
 	}
 
-	@GetMapping(value = ApiPath.SAVED_TRACKS, produces = MediaType.TEXT_HTML_VALUE)
-	public String savedTracksHandler(final HttpSession session, final Model model) {
-		try {
-			model.addAttribute("tracks", savedTrackService.getTracks((String) session.getAttribute("accessToken")));
-		} catch (NoTrackSavedException exception) {
-			return Template.NO_TRACK_SAVED;
-		}
-		return Template.SAVED_TRACKS;
-	}
-
-	@GetMapping(value = ApiPath.SAVED_ALBUMS, produces = MediaType.TEXT_HTML_VALUE)
-	public String savedAlbumsHandler(final HttpSession session, final Model model) {
-		try {
-			model.addAttribute("albums", savedAlbumService.getAlbums((String) session.getAttribute("accessToken")));
-		} catch (NoAlbumSavedException exception) {
-			return Template.NO_ALBUM_SAVED;
-		}
-		return Template.SAVED_ALBUMS;
-	}
-
-	@GetMapping(value = ApiPath.NEW_RELEASE, produces = MediaType.TEXT_HTML_VALUE)
-	public String newReleasesHandler(final HttpSession session, final Model model) {
-		model.addAttribute("releases", newReleasedService.getReleases((String) session.getAttribute("accessToken")));
-		return Template.NEW_RELEASES;
-	}
-
-	@GetMapping(value = ApiPath.RECENT_TRACKS, produces = MediaType.TEXT_HTML_VALUE)
-	public String recentTracksHandler(final HttpSession session, final Model model) {
-		model.addAttribute("tracks", recentPlayedTrackService.getHistory((String) session.getAttribute("accessToken")));
-		return Template.RECENT_TRACKS;
-	}
-
-	@GetMapping(value = ApiPath.FEATURED_PLAYLIST, produces = MediaType.TEXT_HTML_VALUE)
-	public String featuredPlaylistsHandler(final HttpSession session, final Model model) {
-		model.addAttribute("playlists",
-				featuredPlaylistService.getPlaylists((String) session.getAttribute("accessToken")));
-		return Template.FEATURED_PLAYLISTS;
-	}
-
 }
