e7273b9b28d4b2234e4f6ecffd5ee292efc585b0
Benjamin J Hicks <73999671+HennyBicks01@users.noreply.github.com>
2023-07-24 18:18:22 -0400

    no account data exception updated

--- SpotifyTop/src/main/java/FinalProject/SpotifyTop/exception/NoAccountDataException.java
+++ SpotifyTop/src/main/java/FinalProject/SpotifyTop/exception/NoAccountDataException.java
@@ -1,7 +1,9 @@
 package finalproject.spotifytop.exception;
 
+// Custom exception class that extends the RuntimeException class
 public class NoAccountDataException extends RuntimeException {
 
-	private static final long serialVersionUID = -7440409978196904718L;
+    // A unique identifier for serialization purposes
+    private static final long serialVersionUID = -7440409978196904718L;
 
 }
