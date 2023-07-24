5ebc4ef54faed707c22992e76988c59ccb0dfb5d
Benjamin J Hicks <73999671+HennyBicks01@users.noreply.github.com>
2023-07-24 17:53:33 -0400

    Updated ApiPath.java

--- SpotifyTop/src/main/java/FinalProject/SpotifyTop/constant/ApiPath.java
+++ SpotifyTop/src/main/java/FinalProject/SpotifyTop/constant/ApiPath.java
@@ -1,18 +1,42 @@
+// Defines a set of constants for API paths for Spotify application
 package finalproject.spotifytop.constant;
 
 public class ApiPath {
 
-	public static final String CALLBACK = "/callback";
-	public static final String FEATURED_PLAYLIST = "/featuredPlaylists";
-	public static final String LOGOUT = "/logout";
-	public static final String ERROR = "/error";
-	public static final String NEW_RELEASE = "/newReleases";
-	public static final String RECENT_TRACKS = "/recentTracks";
-	public static final String REDIRECT = "/redirect";
-	public static final String SAVED_ALBUMS = "/savedAlbums";
-	public static final String SAVED_TRACKS = "/savedTracks";
-	public static final String SEARCH = "/search";
-	public static final String TOP_ARTISTS = "/topArtists";
-	public static final String TOP_TRACKS = "/topTracks";
+    // Represents the API path for the callback endpoint
+    public static final String CALLBACK = "/callback";
+
+    // Represents the API path for the featured playlists endpoint
+    public static final String FEATURED_PLAYLIST = "/featuredPlaylists";
+
+    // Represents the API path for the logout endpoint
+    public static final String LOGOUT = "/logout";
+
+    // Represents the API path for the error endpoint
+    public static final String ERROR = "/error";
+
+    // Represents the API path for the new releases endpoint
+    public static final String NEW_RELEASE = "/newReleases";
+
+    // Represents the API path for the recent tracks endpoint
+    public static final String RECENT_TRACKS = "/recentTracks";
+
+    // Represents the API path for the redirect endpoint
+    public static final String REDIRECT = "/redirect";
+
+    // Represents the API path for the saved albums endpoint
+    public static final String SAVED_ALBUMS = "/savedAlbums";
+
+    // Represents the API path for the saved tracks endpoint
+    public static final String SAVED_TRACKS = "/savedTracks";
+
+    // Represents the API path for the search endpoint
+    public static final String SEARCH = "/search";
+
+    // Represents the API path for the top artists endpoint
+    public static final String TOP_ARTISTS = "/topArtists";
+
+    // Represents the API path for the top tracks endpoint
+    public static final String TOP_TRACKS = "/topTracks";
 
 }
