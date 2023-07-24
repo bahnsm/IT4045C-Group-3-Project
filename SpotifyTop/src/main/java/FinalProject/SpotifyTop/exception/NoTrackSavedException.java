e458b6787a176a75436d55aed71d171fe356c748
Benjamin J Hicks <73999671+HennyBicks01@users.noreply.github.com>
2023-07-24 18:20:27 -0400

    removed unneeded exception

--- SpotifyTop/src/main/java/FinalProject/SpotifyTop/exception/NoTrackSavedException.java
+++
@@ -1,7 +0,0 @@
-package finalproject.spotifytop.exception;
-
-public class NoTrackSavedException extends RuntimeException {
-
-	private static final long serialVersionUID = 5618484070087795085L;
-
-}
