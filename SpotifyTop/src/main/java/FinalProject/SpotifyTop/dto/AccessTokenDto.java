69325cae39d05010ad2c42f4be4aaba0218f0d8e
Benjamin J Hicks <73999671+HennyBicks01@users.noreply.github.com>
2023-07-24 18:14:25 -0400

    updated lombok dto c.e.u

--- SpotifyTop/src/main/java/FinalProject/SpotifyTop/dto/AccessTokenDto.java
+++ SpotifyTop/src/main/java/FinalProject/SpotifyTop/dto/AccessTokenDto.java
@@ -1,14 +1,21 @@
 package finalproject.spotifytop.dto;
-
+// Lombok annotations for generating getter methods, constructor with all arguments, and default constructor
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+// Lombok annotation to automatically generate getter methods for the class
 @Getter
+
+// Lombok annotation to generate a constructor with all arguments
 @AllArgsConstructor
+
+// Lombok annotation to generate a default constructor
 @NoArgsConstructor
+
 public class AccessTokenDto {
 
-	private String access_token;
+    // Private field representing the access token obtained from Spotify API
+    private String access_token;
 
 }
