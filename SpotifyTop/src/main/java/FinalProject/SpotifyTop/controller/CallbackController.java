7fe33b769bb3e33988d23b60cddc5f5f9b84fc9a
Benjamin J Hicks <73999671+HennyBicks01@users.noreply.github.com>
2023-07-24 18:00:38 -0400

    updated callback controller

--- SpotifyTop/src/main/java/FinalProject/SpotifyTop/controller/CallbackController.java
+++ SpotifyTop/src/main/java/FinalProject/SpotifyTop/controller/CallbackController.java
@@ -22,34 +22,48 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class CallbackController {
 
-	private final SpotifyUrlService spotifyUrlService;
-	private final AccessTokenService accessToken;
-	private final ProfileDetailService userDetails;
-	private final CurrentPlayingService currentPlaying;
+    // Injects necessary services using constructor-based dependency injection
+    private final SpotifyUrlService spotifyUrlService;
+    private final AccessTokenService accessToken;
+    private final ProfileDetailService userDetails;
+    private final CurrentPlayingService currentPlaying;
 
-	@GetMapping(value = ApiPath.CALLBACK, produces = MediaType.TEXT_HTML_VALUE)
-	public String handleCallback(@RequestParam(value = "code", required = false) final String code,
-			@RequestParam(value = "error", required = false) final String error, final Model model,
-			final HttpSession session) {
+    // Handles GET requests to the '/callback' path with the specified media type
+    @GetMapping(value = ApiPath.CALLBACK, produces = MediaType.TEXT_HTML_VALUE)
+    public String handleCallback(@RequestParam(value = "code", required = false) final String code,
+            @RequestParam(value = "error", required = false) final String error, final Model model,
+            final HttpSession session) {
 
-		if (error != null) {
-			model.addAttribute("url", spotifyUrlService.getAuthorizationURL());
-			return Template.CALLBACK_FAILURE;
-		}
-		session.setAttribute("code", code);
-		String token = accessToken.getToken(code);
+        // Check if an error occurred during the authentication process
+        if (error != null) {
+            // If there's an error, add the authorization URL to the model and return the failure template
+            model.addAttribute("url", spotifyUrlService.getAuthorizationURL());
+            return Template.CALLBACK_FAILURE;
+        }
 
-		session.setAttribute("accessToken", token);
-		model.addAttribute("accessToken", token);
-		model.addAttribute("userName", userDetails.getUsername(token));
+        // If there's no error, store the received code in the session
+        session.setAttribute("code", code);
 
-		try {
-			model.addAttribute("currentPlaying", currentPlaying.getCurrentPlaying(token));
-			model.addAttribute("display", 1);
-		} catch (NoTrackPlayingException exception) {
-			model.addAttribute("display", 0);
-		}
+        // Retrieve the access token using the received code
+        String token = accessToken.getToken(code);
 
-		return Template.CALLBACK_SUCCESS;
-	}
+        // Store the access token in the session and add it to the model
+        session.setAttribute("accessToken", token);
+        model.addAttribute("accessToken", token);
+
+        // Retrieve the username of the user associated with the access token and add it to the model
+        model.addAttribute("userName", userDetails.getUsername(token));
+
+        try {
+            // Try to retrieve information about the currently playing track and add it to the model
+            model.addAttribute("currentPlaying", currentPlaying.getCurrentPlaying(token));
+            model.addAttribute("display", 1); // Set a flag to indicate that a track is currently playing
+        } catch (NoTrackPlayingException exception) {
+            // If there's no track currently playing, set the flag to indicate that and handle the exception
+            model.addAttribute("display", 0);
+        }
+
+        // Return the success template
+        return Template.CALLBACK_SUCCESS;
+    }
 }
