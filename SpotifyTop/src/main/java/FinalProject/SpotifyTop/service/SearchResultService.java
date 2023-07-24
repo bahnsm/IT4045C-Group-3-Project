a379e86bf3110a95c49cd5dc18fae13304e87f86
Benjamin J Hicks <73999671+HennyBicks01@users.noreply.github.com>
2023-07-24 18:47:32 -0400

    added null search check

--- SpotifyTop/src/main/java/FinalProject/SpotifyTop/service/SearchResultService.java
+++ SpotifyTop/src/main/java/FinalProject/SpotifyTop/service/SearchResultService.java
@@ -3,6 +3,7 @@ package finalproject.spotifytop.service;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -19,56 +20,89 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SearchResultService {
 
-	private final RestTemplate restTemplate;
-	private static final String URL = "https://api.spotify.com/v1/search?q=SEARCH_QUERY&type=album,artist,playlist,track&limit=3";
+    private final RestTemplate restTemplate;
+    private static final String URL = "https://api.spotify.com/v1/search?q=SEARCH_QUERY&type=album,artist,playlist,track&limit=3";
 
-	public Object search(String token, String searchQuery) {
+    /**
+     * Performs a search on the Spotify API based on the given search query.
+     *
+     * @param token       The access token required for authentication with the Spotify API.
+     * @param searchQuery The search query to perform the search.
+     * @return The search result as a LinkedHashMap containing the response from the Spotify API.
+     * @throws InvalidSearchException if the search query is invalid or the result is empty.
+     */
+    public LinkedHashMap<String, Object> search(String token, String searchQuery) throws InvalidSearchException {
+        if (!isValid(token, searchQuery)) {
+            throw new InvalidSearchException();
+        }
 
-		if (!isValid(token, searchQuery)) {
-			throw new InvalidSearchException();
-		}
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + token);
 
-		HttpHeaders headers = new HttpHeaders();
-		headers.set("Authorization", "Bearer " + token);
+        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
 
-		HttpEntity<String> entity = new HttpEntity<>("paramters", headers);
+        // Perform the search with the formatted URL
+        ResponseEntity<LinkedHashMap<String, Object>> response = restTemplate.exchange(
+                URL.replace("SEARCH_QUERY", SearchQueryFormatter.format(searchQuery)),
+                HttpMethod.GET,
+                entity,
+                new ParameterizedTypeReference<LinkedHashMap<String, Object>>() {}
+        );
 
-		ResponseEntity<Object> response = restTemplate.exchange(
-				URL.replace("SEARCH_QUERY", SearchQueryFormatter.format(searchQuery)), HttpMethod.GET, entity,
-				Object.class);
-		LinkedHashMap result = (LinkedHashMap) response.getBody();
+        // Retrieve the response body directly as LinkedHashMap<String, Object>
+        LinkedHashMap<String, Object> result = response.getBody();
 
-		return result;
-	}
+        // Handle the case where the response is null (e.g., API issue)
+        if (result == null) {
+            throw new RuntimeException("Search result is null or empty.");
+        }
 
-	public boolean isValid(String token, String searchQuery) {
-		HttpHeaders headers = new HttpHeaders();
-		headers.set("Authorization", "Bearer " + token);
+        return result;
+    }
 
-		HttpEntity<String> entity = new HttpEntity<>("paramters", headers);
+    /**
+     * Checks if the search query is valid and returns true if there are any results.
+     *
+     * @param token       The access token required for authentication with the Spotify API.
+     * @param searchQuery The search query to perform the search.
+     * @return true if the search query is valid and there are any results, false otherwise.
+     */
+    public boolean isValid(String token, String searchQuery) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + token);
 
-		ResponseEntity<Object> response = restTemplate.exchange(
-				URL.replace("SEARCH_QUERY", SearchQueryFormatter.format(searchQuery)), HttpMethod.GET, entity,
-				Object.class);
-		LinkedHashMap result = (LinkedHashMap) response.getBody();
+        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
 
-		LinkedHashMap albums = (LinkedHashMap) result.get("albums");
-		ArrayList itemsAlbums = (ArrayList) albums.get("items");
+        // Perform the search with the formatted URL
+        ResponseEntity<LinkedHashMap<String, Object>> response = restTemplate.exchange(
+                URL.replace("SEARCH_QUERY", SearchQueryFormatter.format(searchQuery)),
+                HttpMethod.GET,
+                entity,
+                new ParameterizedTypeReference<LinkedHashMap<String, Object>>() {}
+        );
 
-		LinkedHashMap artists = (LinkedHashMap) result.get("artists");
-		ArrayList itemsArtists = (ArrayList) albums.get("items");
+        // Retrieve the response body directly as LinkedHashMap<String, Object>
+        LinkedHashMap<String, Object> result = response.getBody();
 
-		LinkedHashMap tracks = (LinkedHashMap) result.get("tracks");
-		ArrayList itemsTracks = (ArrayList) albums.get("items");
+		    // Check if the result is null (no search results)
+			if (result == null) {
+				return false;
+			}
 
-		LinkedHashMap playlists = (LinkedHashMap) result.get("playlists");
-		ArrayList itemsPlaylists = (ArrayList) albums.get("items");
+        // Check if there are any results in albums, artists, playlists, or tracks
+        LinkedHashMap<?, ?> albums = (LinkedHashMap<?, ?>) result.get("albums");
+        ArrayList<?> itemsAlbums = (ArrayList<?>) albums.get("items");
 
-		if ((itemsAlbums.size() + itemsArtists.size() + itemsPlaylists.size() + itemsTracks.size()) == 0) {
-			return false;
-		}
+        LinkedHashMap<?, ?> artists = (LinkedHashMap<?, ?>) result.get("artists");
+        ArrayList<?> itemsArtists = (ArrayList<?>) artists.get("items");
 
-		return true;
+        LinkedHashMap<?, ?> tracks = (LinkedHashMap<?, ?>) result.get("tracks");
+        ArrayList<?> itemsTracks = (ArrayList<?>) tracks.get("items");
 
-	}
+        LinkedHashMap<?, ?> playlists = (LinkedHashMap<?, ?>) result.get("playlists");
+        ArrayList<?> itemsPlaylists = (ArrayList<?>) playlists.get("items");
+		
+
+        return (itemsAlbums.size() + itemsArtists.size() + itemsPlaylists.size() + itemsTracks.size()) > 0;
+    }
 }
